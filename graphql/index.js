c7475371f13a8392657fa1621095264b10b0f456
Hai Dang <nhaidang2808@gmail.com>
2021-09-09 17:33:35 +0700

    typeDefs

--- graphql/index.js
+++ graphql/index.js
@@ -1,12 +1,115 @@
-var GraphQLSchema = require('graphql').GraphQLSchema;
-var GraphQLObjectType = require('graphql').GraphQLObjectType;
-var queryType = require('./queries/user').queryType;
-var mutation = require('./mutations/index');
+// var GraphQLSchema = require('graphql').GraphQLSchema;
+// var GraphQLObjectType = require('graphql').GraphQLObjectType;
+// var queryType = require('./queries/user').queryType;
+// var mutation = require('./mutations/index');
 
-exports.noteSchema = new GraphQLSchema({
-  query: queryType,
-  mutation: new GraphQLObjectType({
-    name: 'Mutation',
-    fields: mutation
-  })
-})
+// exports.noteSchema = new GraphQLSchema({
+//   query: queryType,
+//   mutation: new GraphQLObjectType({
+//     name: 'Mutation',
+//     fields: mutation
+//   })
+// })
+
+// var GraphQLSchema = require('graphql').GraphQLSchema;
+// var GraphQLObjectType = require('graphql').GraphQLObjectType;
+// var queryType = require('./queries/user').queryType;
+// var mutation = require('./mutations/index');
+var { gql } = require('apollo-server-express');
+const NoteModel = require('../models/user.js');
+// exports.noteSchema = new GraphQLSchema({
+//   query: queryType,
+//   mutation: new GraphQLObjectType({
+//     name: 'Mutation',
+//     fields: mutation
+//   })
+// })
+
+exports.typedef = gql`
+
+  type Query {
+    notes: [NoteType]
+    noteDetail(id: String):NoteType
+  }
+
+  type Mutation {
+    addNote(
+      name: String!
+      content: String
+    ): NoteType
+    removeNote(id: String!): NoteType
+    updateNote(
+      id: String
+      name: String
+      content: String
+    ): NoteType
+  }
+
+
+  input UserInputType {
+      name: String
+      id: String
+  }
+  type UserType {
+      name: String
+      id: String
+  }
+  type NoteType {
+    "id note"
+    id: String
+    "name note"
+    name: String
+    # this is commment
+    content: String
+    user: UserType
+  }
+`;
+
+
+exports.resolver = {
+  Query: {
+    notes: () => {
+      const notes = NoteModel.find().exec()
+      if (!notes) {
+        throw new Error('Error')
+      }
+      return notes
+    },
+    noteDetail: (root, args, context) => {
+      try {
+        return NoteModel.findById(noteId);
+      } catch (error) {
+        
+      }
+    }
+  },
+  Mutation: {
+    addNote: (root, params) => {
+      try {
+        const uModel = new NoteModel(params);
+        return uModel.save();
+      } catch (error) {
+        return error;
+      }
+    },
+    removeNote: async (root, params) => {
+      try {
+        const newNote = await uModel.save();
+        if (!newNote) {
+          throw new Error('Error');
+        }
+        return newNote
+      } catch (error) {
+        return error;
+      }
+    },
+    updateNote: (root, params) => {
+      const uModel = new NoteModel(params);
+      const newNote = uModel.save();
+      if (!newNote) {
+        throw new Error('Error');
+      }
+      return newNote
+    },
+  },
+}
--- server.js
+++ server.js
@@ -1,20 +1,72 @@
-const express = require("express");
+// const express = require("express");
+// const mongoose = require('./config/mongoose');
+// const graphqlHTTP = require('express-graphql').graphqlHTTP;
+// const cors = require("cors");
+// const db = mongoose();
+// const app = express();
+
+// app.use('*', cors());
+
+// const noteSchema = require('./graphql/index').noteSchema;
+// app.use('/graphql', cors(), graphqlHTTP({
+//   schema: noteSchema,
+//   rootValue: global,
+//   graphiql: true
+// }));
+
+// // Up and Running at Port 4000
+// app.listen(process.env.PORT || 4000, () => {
+//   console.log('A GraphQL API running at port 4000');
+// });
+
+// const express = require("express");
+// const mongoose = require('./config/mongoose');
+// const graphqlHTTP = require('express-graphql').graphqlHTTP;
+// const cors = require("cors");
+// const db = mongoose();
+// const app = express();
+
+// app.use('*', cors());
+
+
+// const { makeExecutableSchema } = require('apollo-server-express');
+// const { applyMiddleware } = require('graphql-middleware');
+
+
+// app.use('/graphql', graphqlHTTP({
+//   schema: typedef,
+//   rootValue: resolver,
+//   graphiql: true
+// }));
+
+// // Up and Running at Port 4000
+// app.listen(process.env.PORT || 4000, () => {
+//   console.log('A GraphQL API running at port 4000');
+// });
+
+const { ApolloServer } = require('apollo-server-express');
+const { ApolloServerPluginDrainHttpServer } = require('apollo-server-core');
+const express = require('express');
+const http = require('http');
+const { typedef } = require('./graphql/index');
+const { resolver } = require('./graphql/index');
 const mongoose = require('./config/mongoose');
-const graphqlHTTP = require('express-graphql').graphqlHTTP;
-const cors = require("cors");
-const db = mongoose();
-const app = express();
 
-app.use('*', cors());
 
-const noteSchema = require('./graphql/index').noteSchema;
-app.use('/graphql', cors(), graphqlHTTP({
-  schema: noteSchema,
-  rootValue: global,
-  graphiql: true
-}));
+async function startApolloServer(typedef, resolver) {
+  const app = express();
+  const httpServer = http.createServer(app);
+  const server = new ApolloServer({
+    typeDefs: typedef,
+    resolvers: resolver,
+    plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
+    graphiql: true
+  });
+  await server.start();
+  server.applyMiddleware({ app });
+  await new Promise(resolve => httpServer.listen({ port: 4000 }, resolve));
+  mongoose()
+  console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`);
+}
 
-// Up and Running at Port 4000
-app.listen(process.env.PORT || 4000, () => {
-  console.log('A GraphQL API running at port 4000');
-});
+startApolloServer(typedef, resolver);
