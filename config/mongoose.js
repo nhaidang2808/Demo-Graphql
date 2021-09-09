e8c9cec4e740f4a67a104b5fecc95b1fb77b415a
Hai Dang <nhaidang2808@gmail.com>
2021-09-09 14:56:12 +0700

    Fix db

--- config/mongoose.js
+++ config/mongoose.js
@@ -6,7 +6,7 @@ module.exports = function () {
 mongoose.Promise = global.Promise;
 var db = mongoose.connect(config.db);
 mongoose.connection.on('error', function (err) {
-    console.log('Error: Could not connect to MongoDB. Did you forget to run `mongod`?'.red);
+    console.log('Error: Could not connect to MongoDB. Did you forget to run `mongodb`?'.red);
 }).on('open', function () {
     console.log('Connection extablised with MongoDB')
 })
