e0506dfeb820c319ddcfcbe108ce67157226a459
Hai Dang <nhaidang2808@gmail.com>
2021-09-10 11:49:05 +0700

    update typeDefs

--- graphql/index.js
+++ graphql/index.js
@@ -1,35 +1,11 @@
-// var GraphQLSchema = require('graphql').GraphQLSchema;
-// var GraphQLObjectType = require('graphql').GraphQLObjectType;
-// var queryType = require('./queries/user').queryType;
-// var mutation = require('./mutations/index');
-
-// exports.noteSchema = new GraphQLSchema({
-//   query: queryType,
-//   mutation: new GraphQLObjectType({
-//     name: 'Mutation',
-//     fields: mutation
-//   })
-// })
-
-// var GraphQLSchema = require('graphql').GraphQLSchema;
-// var GraphQLObjectType = require('graphql').GraphQLObjectType;
-// var queryType = require('./queries/user').queryType;
-// var mutation = require('./mutations/index');
 var { gql } = require('apollo-server-express');
 const NoteModel = require('../models/user.js');
-// exports.noteSchema = new GraphQLSchema({
-//   query: queryType,
-//   mutation: new GraphQLObjectType({
-//     name: 'Mutation',
-//     fields: mutation
-//   })
-// })
+
 
 exports.typedef = gql`
 
   type Query {
     notes: [NoteType]
-    noteDetail(id: String):NoteType
   }
 
   type Mutation {
@@ -39,21 +15,12 @@ exports.typedef = gql`
     ): NoteType
     removeNote(id: String!): NoteType
     updateNote(
-      id: String
-      name: String
+      id: String!
+      name: String!
       content: String
-    ): NoteType
+      ): NoteType
   }
 
-
-  input UserInputType {
-      name: String
-      id: String
-  }
-  type UserType {
-      name: String
-      id: String
-  }
   type NoteType {
     "id note"
     id: String
@@ -61,7 +28,6 @@ exports.typedef = gql`
     name: String
     # this is commment
     content: String
-    user: UserType
   }
 `;
 
@@ -75,13 +41,6 @@ exports.resolver = {
       }
       return notes
     },
-    noteDetail: (root, args, context) => {
-      try {
-        return NoteModel.findById(noteId);
-      } catch (error) {
-        
-      }
-    }
   },
   Mutation: {
     addNote: (root, params) => {
@@ -92,24 +51,20 @@ exports.resolver = {
         return error;
       }
     },
-    removeNote: async (root, params) => {
+    removeNote: (root, params) => {
       try {
-        const newNote = await uModel.save();
-        if (!newNote) {
-          throw new Error('Error');
-        }
-        return newNote
+        const removednote = NoteModel.findByIdAndRemove(params.id).exec();
+        return removednote;
       } catch (error) {
         return error;
       }
     },
     updateNote: (root, params) => {
-      const uModel = new NoteModel(params);
-      const newNote = uModel.save();
-      if (!newNote) {
-        throw new Error('Error');
+      try {
+        return NoteModel.findByIdAndUpdate(params.id,params, {new: true}).exec();
+      } catch (error) {
+        return error;
       }
-      return newNote
     },
   },
 }
