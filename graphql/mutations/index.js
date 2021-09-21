246da24920b07a9fab303e596acd587a5df01051
Hai Dang <nhaidang2808@gmail.com>
2021-09-21 17:51:07 +0700

    delete old Rest API

--- graphql/mutations/index.js
+++
@@ -1,9 +0,0 @@
-var addNote = require('./add').add;
-var removeNote = require('./remove').remove;
-var updateNote = require('./update').update;
-
-module.exports = {
-  addNote,
-  removeNote,
-  updateNote
-}
